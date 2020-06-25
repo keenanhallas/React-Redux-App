e3e0b54d421ff174997a7e8c4be013257fbc790c
Keenan <keenanhallas@gmail.com>
2020-06-25 16:07:22 -0400

    reducer cases written

---
+++ async-redux-app/src/reducers/index.js
@@ -0,0 +1,34 @@
+const initialState = {
+    genre: null,
+    isFetching: false,
+    isLoading: false,
+    error: ""
+}
+
+const appReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "FETCHING_GENRE_START":
+            return {
+                ...state,
+                isFetching: true,
+                isLoading: true,
+                error: ""
+            };
+        case "FETCHING_GENRE_SUCCESS":
+            return {
+                ...state,
+                genre: action.payload,
+                isFetching: false,
+                isLoading: false
+            };
+        case "FETCHING_GENRE_FAILURE":
+            return {
+                ...state,
+                isFetching: false,
+                isLoading: false,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
+}
